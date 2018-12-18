eb307bfdcd141bd99dddf7046a211e35952cb694
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-12-18 14:28:26 +0400

    --

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const next = require('next');
 const url = require('url');
 const path = require('path');
 const http = require('http');
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV != 'production';
@@ -10,20 +13,42 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    http.createServer((req, res) => {
 
-        const parsedUrl = url.parse(req.url, true);
+    const server = express();
 
-        const { pathname } = parsedUrl;
 
-        if (pathname === '/service-worker.js') {
-            const filepath = path.join(__dirname, '.next', pathname);
-            app.serveStatic(req, res, filepath);
-        } else {
-            handle(req, res, parsedUrl);
+    server.use('/graphql', expressGraphQL({
+        schema,
+        graphiql: true
+    }));
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(port, err => {
+
+        if (err) {
+            throw err;
         }
 
-    }).listen(port, () => {
         console.log(`Listening on port ${port}`);
-    });
+    })
+
+    // http.createServer((req, res) => {
+
+    //     const parsedUrl = url.parse(req.url, true);
+
+    //     const { pathname } = parsedUrl;
+
+    //     if (pathname === '/service-worker.js') {
+    //         const filepath = path.join(__dirname, '.next', pathname);
+    //         app.serveStatic(req, res, filepath);
+    //     } else {
+    //         handle(req, res, parsedUrl);
+    //     }
+
+    // }).listen(port, () => {
+    //     console.log(`Listening on port ${port}`);
+    // });
 });
